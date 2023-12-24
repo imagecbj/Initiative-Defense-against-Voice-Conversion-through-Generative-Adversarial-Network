a4ab56040e02cac470d917b95a0187de24996216
predawn <predawnang@163.com>
2023-12-24 10:47:52 +0800

    samples read me

---
+++ samples/readme.md
@@ -0,0 +1,4 @@
+Naming conventions with prefixes are used to differentiate various types of audio files.
+
+1. The prefix t\_ indicates the target audio. For example, the file with the name 't_p236_167.wav' means that it is a target audio where the speaker is p236, and 167 serves as a more specific indicator for the audio.
+2. The prefix `s_*` indicates the source audio. for example the fi
--- train_swcsm.py
+++ train_swcsm.py
@@ -31,12 +31,14 @@ def prepare_rec_mels():
 
 
 class DistortionDataset(Dataset):
-    def __init__(self, metadata_tag='train'):
+    def __init__(self, metadata_tag="train"):
         self.hp = hp
         with open(self.hp.metadata, "r") as j:
             self.metadata = json.load(j)[metadata_tag]
 
-    def __len__(self, ):
+    def __len__(
+        self,
+    ):
         return len(self.metadata)
 
     def __getitem__(self, idx):
@@ -44,18 +46,18 @@ class DistortionDataset(Dataset):
         mel_path = Path(mel_path)
         spk = mel_path.parent.name
 
-        rec_mel_path = mel_path.parents[2] / 'work_1_rec_mels' / spk / mel_path.name
+        rec_mel_path = mel_path.parents[2] / "work_1_rec_mels" / spk / mel_path.name
         mel = torch.load(mel_path)
         rec_mel = torch.load(rec_mel_path)
 
         if len(mel) < self.hp.seg_len:
             len_pad = self.hp.seg_len - len(mel)
-            mel = pad(mel, (0, 0, 0, len_pad), 'constant', 0)
-            rec_mel = pad(rec_mel, (0, 0, 0, len_pad), 'constant', 0)
+            mel = pad(mel, (0, 0, 0, len_pad), "constant", 0)
+            rec_mel = pad(rec_mel, (0, 0, 0, len_pad), "constant", 0)
         else:
             start = random.randint(0, max(len(mel) - self.hp.seg_len, 0))
-            mel = mel[start: (start + self.hp.seg_len)]
-            rec_mel = rec_mel[start: (start + self.hp.seg_len)]
+            mel = mel[start : (start + self.hp.seg_len)]
+            rec_mel = rec_mel[start : (start + self.hp.seg_len)]
 
         mel = mel.T
         rec_mel = rec_mel.T
@@ -65,7 +67,7 @@ class DistortionDataset(Dataset):
 
 if __name__ == "__main__":
     dataset = DistortionDataset()
-    testset = DistortionDataset('test')
+    testset = DistortionDataset("test")
     mse = nn.MSELoss()
     dataloader = DataLoader(dataset, batch_size=128, shuffle=True, drop_last=True)
     testloader = DataLoader(testset, batch_size=1024, shuffle=False, drop_last=True)
@@ -73,7 +75,7 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(g.parameters(), lr=5e-5)
     best = 0
     loss_per_epochs = []
-    for epoch in range(1000):
+    for epoch in range(500):
         g.train()
         for i, (mel, rec_mel) in tqdm(enumerate(dataloader)):
             mel = mel.to(hp.device)
@@ -81,7 +83,7 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             distored_mel = g(mel)
             loss = mse(denormalizer(distored_mel), denormalizer(rec_mel))
-            print(f'loss: {loss.item()}\r')
+            print(f"loss: {loss.item()}\r")
             loss.backward()
             optimizer.step()
 
@@ -98,5 +100,7 @@ if __name__ == "__main__":
             if loss_per_epochs[epoch] < loss_per_epochs[best]:
                 best = epoch
 
-        torch.save(g.state_dict(), f'epoch_{epoch}_d_net.pt')
-        print(f'{"*" * 10}test done, :BEST: {best} value: {loss_per_epochs[best]} {"*" * 10}')
+        torch.save(g.state_dict(), f"swcsm/epoch_{epoch}_d_net.pt")
+        print(
+            f'{"*" * 10}test done, :BEST: {best} value: {loss_per_epochs[best]} {"*" * 10}'
+        )
